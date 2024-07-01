6854dfb18688a1d7b69039eadff8af37924cc14a
Wisarut Hakaen <bukobeer001@gmail.com>
2024-07-02 00:45:16 +0700

    update i18n for housing

--- src/page/housing/HousingPage.js
+++ src/page/housing/HousingPage.js
@@ -124,7 +124,7 @@ const HousingPage = () => {
                 <div className="relative m-5 w-2/4">
                     <div className="flex items-center  justify-end px-1 py-3">
                         <button
-                            onClick={(e) => setPage('addhousing')}
+                            onClick={() => setPage('addhousing')}
                             className="text-white bg-green-600 hover:bg-green-700 focus:ring-green-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
                             {t('form.header.housing.add')}
                         </button>
